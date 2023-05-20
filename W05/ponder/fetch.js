2f5bf3a227f1140d677a0f16132f90d71be5ba14
trrmann <tracy.mann1@retailbusinessservices.com>
2023-05-20 18:37:23 -0400

    complete activity 2

--- W05/ponder/fetch.js
+++ W05/ponder/fetch.js
@@ -1,5 +1,6 @@
 // fetch.js
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 async function getPokemon(url) {
   const response = await fetch(url);
@@ -20,5 +21,23 @@ function doStuff(data) {
   element.innerHTML = html;
   console.log("first: ", results);
 }
+async function getPokemonList(url) {
+    const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuffList(data);
+  }
+}
+function doStuffList(data) {
+    elementList = document.querySelector("#outputList")
+    pokeList = data.results;
+    const pokeArray = pokeList.map((poke) => {const liElement = document.createElement("li"); liElement.innerText = poke.name;return liElement});
+    pokeArray.map((element) => {elementList.appendChild(element)});
+
+}
 getPokemon(url);
+getPokemonList(urlList);
 console.log("second: ", results);
