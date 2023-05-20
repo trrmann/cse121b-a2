8cf6ed5edf7373cf51adb0701d2ac621fdaa06f3
trrmann <tracy.mann1@retailbusinessservices.com>
2023-05-20 19:20:35 -0400

    complete solution 2 class alt

--- W05/ponder/fetch.js
+++ W05/ponder/fetch.js
@@ -3,40 +3,31 @@ const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 const urlList = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 
-function getPokemon(url, isList=0) {
-  if(isList!=0) {
-    isList=1;
-  }
-  if(isList==1) {
-    fetch(url).then(response => doStuffList(response));
-  } else {
-    fetch(url).then(response => doStuff(response));
-  }
+async function getPokemon(url, doThis) {
+    const response = await fetch(url);
+    if(response.ok) {
+        const data = await response.json();
+        doThis(data);
+    }
 }
 
-async function doStuff(response) {
-    if(response.ok) {
-        element = document.querySelector("#output")
-        const data = await response.json();
-        results = data;
-        elementHTML = `${JSON.stringify(results)}`;
-        const html = `<h2>${results.name}</h2>
+function doStuff(data) {
+    element = document.querySelector("#output")
+    results = data;
+    elementHTML = `${JSON.stringify(results)}`;
+    const html = `<h2>${results.name}</h2>
                     <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
-        element.innerHTML = html;
-        console.log("first: ", results);    
-    }
+    element.innerHTML = html;
+    console.log("first: ", results);    
 }
 
-async function doStuffList(response) {
-    if(response.ok) {
-        elementList = document.querySelector("#outputList")
-        const data = await response.json();
-        pokeList = data.results;
-        const pokeArray = pokeList.map((poke) => {const liElement = document.createElement("li"); liElement.innerText = poke.name;return liElement});
-        pokeArray.map((element) => {elementList.appendChild(element)});
-    }
+function doStuffList(data) {
+    elementList = document.querySelector("#outputList")
+    pokeList = data.results;
+    const pokeArray = pokeList.map((poke) => {const liElement = document.createElement("li"); liElement.innerText = poke.name;return liElement});
+    pokeArray.map((element) => {elementList.appendChild(element)});
 }
 
-getPokemon(url);
-getPokemon(urlList, 1);
+getPokemon(url, doStuff);
+getPokemon(urlList, doStuffList);
 console.log("second: ", results);
