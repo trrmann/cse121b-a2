0a5fc15e1c9633e3d00c7532085b047c9a69db15
trrmann <tracy.mann1@retailbusinessservices.com>
2023-05-20 19:16:23 -0400

    complete my activity 2 alt 2

--- W05/ponder/fetch.js
+++ W05/ponder/fetch.js
@@ -3,39 +3,38 @@ const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 const urlList = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 
-async function getPokemon(url, isList=0) {
+function getPokemon(url, isList=0) {
   if(isList!=0) {
     isList=1;
   }
-  const response = await fetch(url);
-  //check to see if the fetch was successful
-  if (response.ok) {
-    // the API will send us JSON...but we have to convert the response before we can use it
-    // .json() also returns a promise...so we await it as well.
-    const data = await response.json();
-    if(isList==1) {
-        doStuffList(data);
-    } else {
-        doStuff(data);
-    }
+  if(isList==1) {
+    fetch(url).then(response => doStuffList(response));
+  } else {
+    fetch(url).then(response => doStuff(response));
   }
 }
 
-function doStuff(data) {
-    element = document.querySelector("#output")
-    results = data;
-  elementHTML = `${JSON.stringify(results)}`;
-  const html = `<h2>${results.name}</h2>
-                <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
-  element.innerHTML = html;
-  console.log("first: ", results);
+async function doStuff(response) {
+    if(response.ok) {
+        element = document.querySelector("#output")
+        const data = await response.json();
+        results = data;
+        elementHTML = `${JSON.stringify(results)}`;
+        const html = `<h2>${results.name}</h2>
+                    <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
+        element.innerHTML = html;
+        console.log("first: ", results);    
+    }
 }
 
-function doStuffList(data) {
-    elementList = document.querySelector("#outputList")
-    pokeList = data.results;
-    const pokeArray = pokeList.map((poke) => {const liElement = document.createElement("li"); liElement.innerText = poke.name;return liElement});
-    pokeArray.map((element) => {elementList.appendChild(element)});
+async function doStuffList(response) {
+    if(response.ok) {
+        elementList = document.querySelector("#outputList")
+        const data = await response.json();
+        pokeList = data.results;
+        const pokeArray = pokeList.map((poke) => {const liElement = document.createElement("li"); liElement.innerText = poke.name;return liElement});
+        pokeArray.map((element) => {elementList.appendChild(element)});
+    }
 }
 
 getPokemon(url);
